2540c0d4d5d207485a007cb80517853f711a31e9
Daniel Palacio <dpalacio@buffalo.edu>
2019-12-30 03:58:48 -0500

    Initial Commit

---
+++ My Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Journal
+//
+//  Created by Daniel Palacio on 12/30/19.
+//  Copyright © 2019 Daniel Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
